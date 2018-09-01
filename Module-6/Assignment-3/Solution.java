bcab6a668c6d740681cbb3b266f95bfcb5ae121b
= <=>
2018-09-01 10:46:04 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- Module-6/Assignment-3/Solution.java
+++ Module-6/Assignment-3/Solution.java
@@ -26,8 +26,8 @@ final class Solution {
     public static char[] replaceAll(final char[] charArray,
         final char oldChar, final char newChar) {
         char[] newCharArray = new char[charArray.length];
-        for(int i = 0; i < charArray.length; i++) {
-            if(charArray[i] == oldChar) {
+        for (int i = 0; i < charArray.length; i++) {
+            if(charArray[i] == oldChar){
                 newCharArray[i] = newChar;
             } else {
                 newCharArray[i] = charArray[i];
