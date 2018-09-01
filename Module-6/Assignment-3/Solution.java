610ab4705f9a6eebf9e80f12a4fd4ff6c88c1659
= <=>
2018-09-01 11:04:58 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- Module-6/Assignment-3/Solution.java
+++ Module-6/Assignment-3/Solution.java
@@ -25,12 +25,10 @@ final class Solution {
      */
     public static char[] replaceAll(final char[] charArray,
         final char oldChar, final char newChar) {
-        char[] newCharArray = new char[charArray.length];
+        char[] newCharArray = charArray;
         for (int i = 0; i < charArray.length; i++) {
             if (charArray[i] == oldChar) {
                 newCharArray[i] = newChar;
-            } else {
-                newCharArray[i] = charArray[i];
             }
         }
         return newCharArray;
