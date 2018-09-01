8b7a7d90f87b3c2c9dbec5e77e613ad1aa2221c7
= <=>
2018-09-01 10:31:34 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ Module-6/Assignment-2/Solution.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+/**
+ * Write a java program to round the
+ * elements of a matrix to the nearest 100.
+ *
+ * @author : 
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * [roundToHundered description]
+     * @param  n [description]
+     * @return   [description]
+     */
+    public static int roundToHundered(int n) {
+        int diff = 100 - (n % 100);
+        if(diff >= 50) {
+            return n - (100 - diff);
+        } else {
+            return n + diff;
+        }
+    }
+    /**
+     * Function to round the
+     * elements of a matrix to the nearest 100.
+     *
+     * @param      a     Matrix of which the elements to be rounded
+     * @param      rows     Number of rows
+     * @param      columns     Number of columns
+     *
+     * @return     Matrix of the rounded elements
+     */
+    public static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                a[i][j] = roundToHundered(a[i][j]);
+            }
+        }
+        return a;
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+        int[][] b = roundHundred(a, m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println(b[i][n - 1]);
+        }
+    }
+}
