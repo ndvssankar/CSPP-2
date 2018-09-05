44acc3d1bebe767abd79b1d12e60f11a70501a9e
= <=>
2018-09-05 14:23:55 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Module-9/Assignment-1/Fibonacci.java
+++ Module-9/Assignment-1/Fibonacci.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-/*
+/**
  * In the previous module you have created List ADT.
  * ADTs are developed as reusable libraries
  * i.e., anyone who needs the ADT can use it in their program
@@ -17,17 +17,17 @@ import java.util.Scanner;
  *
  */
 
-public class Fibonacci {
+public final class Fibonacci {
 
     /**
-     * [Fibonacci description].
+     * Its a private constructor.
      * @return [description]
      */
     private Fibonacci() {
 
     }
 
-    /*
+    /**
      * fib is a static method takes a argument n
      * n is the count of the fibonacci numbers to be generated.
      * The method returns a List with the n fibonacci numbers.
@@ -45,9 +45,22 @@ public class Fibonacci {
         return fib;
     }
 
+    /**
+     * Main method.
+     * @param args [description]
+     */
 	public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         System.out.println(fib(n));
     }
 }
+
+
+
+
+
+
+
+
+
