80ad0cf810683af8c16183823fd39494d4bdc64a
= <=>
2018-09-06 09:51:03 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- Module-10/Class/Assignment-1/List.java
+++ Module-10/Class/Assignment-1/List.java
@@ -278,6 +278,10 @@ public class List {
         The method returns void (nothing)
      */
     public void add(int index,int item) {
+        if(index < 0) {
+            System.out.println("Negative Index Exception");
+            return;
+        }
          for(int i=size; i>index; i--)
             list[i] = list[i-1];
         list[index] = item;
