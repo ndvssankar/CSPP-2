e4e5e2bf8d47714e7d95219c1316588891e13486
= <=>
2018-09-07 10:38:46 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- Module-11/Assignment-1/List.java
+++ Module-11/Assignment-1/List.java
@@ -175,7 +175,7 @@ public class List {
     public String toString() {
         // Replace the code below
         if (size == 0)
-            return "";
+            return "[]";
         String str = "[";
         int i = 0;
         for (i = 0; i < size - 1; i++) {
