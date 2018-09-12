9d7d3ad4e5a18d1c2917ceaf4532bf9616538738
= <=>
2018-09-12 12:24:58 +0530

    "check_style_score -> 2 of 4 passed. style score: 1/1 "

--- Module-16/bookyourshow/BookYourShow.java
+++ Module-16/bookyourshow/BookYourShow.java
@@ -66,7 +66,7 @@ public class BookYourShow extends Show {
                 flag = true;
             }
         }
-        if (flag) {
+        if (!flag) {
             System.out.println("Invalid");
         }
     }
