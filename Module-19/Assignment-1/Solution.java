3b3018f744e532404d2993001d86790e570cf933
= <=>
2018-09-18 07:29:50 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

--- Module-19/Assignment-1/Solution.java
+++ Module-19/Assignment-1/Solution.java
@@ -11,9 +11,9 @@ public final class Solution {
         // leave this blank
     }
     /**
-     * main function to execute test cases.
-     *
-     * @param      args  The arguments
+     * main to demonstrate quiz
+     * @param  args      command line args.
+     * @throws Exception throws exception.
      */
     public static void main(final String[] args) throws Exception {
         // instantiate this Quiz
@@ -67,9 +67,10 @@ public final class Solution {
      * @param      s              The scanner object for user input
      * @param      quiz           The quiz object
      * @param      questionCount  The question count
+     * @throws Exception throws exception.
      */
-    public static void loadQuestions(final Scanner s, final Quiz quiz, final int questionCount)
-    throws Exception {
+    public static void loadQuestions(final Scanner s, final Quiz quiz,
+                                     final int questionCount) throws Exception {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
@@ -84,11 +85,13 @@ public final class Solution {
     /**
      * Starts a quiz.
      *
-     * @param      s            The scanner object for user input
-     * @param      quiz         The quiz object
-     * @param      answerCount  The answer count
+     * @param      s            The scanner object for user input.
+     * @param      quiz         The quiz object.
+     * @param      answerCount  The answer count.
+     * @throws Exception throws exception.
      */
-    public static void startQuiz(final Scanner s, final Quiz quiz, final int answerCount) {
+    public static void startQuiz(final Scanner s, final Quiz quiz,
+                                 final int answerCount) {
         // write your code here to display the quiz questions
         // read the user responses from the console
         // store the user respones in the quiz object
@@ -96,25 +99,27 @@ public final class Solution {
             System.out.println(quiz.getQuestion(i));
             String choice = s.nextLine();
             String[] choices = quiz.getQuestion(i).getChoices();
-            String str = choices[quiz.getQuestion(i).getCorrectAnswer()-1];
+            String str = choices[quiz.getQuestion(i).getCorrectAnswer() - 1];
             if (choice.equals(str)) {
-                quiz.getQuestion(i).setResponse(quiz.getQuestion(i).getCorrectAnswer());
+                quiz.getQuestion(i).setResponse(quiz.getQuestion(i)
+                                                .getCorrectAnswer());
             }
         }
     }
 
     /**
-     * Displays the score report
+     * Displays the score report.
      *
-     * @param      quiz     The quiz object
+     * @param      quiz     The quiz object.
+     * @throws Exception throws exception.
      */
     public static void displayScore(final Quiz quiz) {
         int totalScore = 0;
         for (int i = 0; i < quiz.size(); i++) {
             Question question = quiz.getQuestion(i);
             System.out.println(question.getQuestionText());
-            // System.out.println(question.getResponse() + " :: " +question.getCorrectAnswer());
-            if (question.getCorrectAnswer() == question.getResponse()) {
+            if (question.getCorrectAnswer() ==
+                    question.getResponse()) {
                 System.out.println(" Correct Answer! - Marks Awarded: "
                                    + question.getMaxMarks());
                 totalScore += question.getMaxMarks();
@@ -127,29 +132,36 @@ public final class Solution {
         System.out.println("Total Score: " + totalScore);
     }
 
+    /**
+     * check if the question is valid or not
+     * @param  question  question.
+     * @return Question  question object.
+     * @throws Exception throws exception.
+     */
     private static Question isValidQuestion(final String question)
     throws Exception {
         String[] tokens = question.split(":");
-        if (tokens.length < 5) {
+        if (tokens.length < 2 + 2 + 1) {
             throw new Exception("Error! Malformed question");
         }
-        String questionText = tokens[0];
+        String qText = tokens[0];
         String[] choices = tokens[1].split(",");
         int correctAnswer = Integer.parseInt(tokens[2]);
-        int maxMarks = Integer.parseInt(tokens[3]);
-        int penalty = Integer.parseInt(tokens[4]);
-        if (questionText.length() == 0) {
+        int maxMarks = Integer.parseInt(tokens[2 + 1]);
+        int penalty = Integer.parseInt(tokens[2 + 2]);
+        if (qText.length() == 0) {
             throw new Exception("Error! Malformed question");
         } else if (choices.length < 2) {
             throw new Exception(tokens[0] + " does not have enough answer choices");
         } else if (correctAnswer < 1 || correctAnswer > choices.length) {
-            throw new Exception("Error! Correct answer choice number is out of range for " + questionText);
+            throw new Exception(
+                "Error! Correct answer choice number is out of range for " + qText);
         } else if (maxMarks < 0) {
-            throw new Exception("Invalid max marks for " + questionText);
+            throw new Exception("Invalid max marks for " + qText);
         } else if (penalty > 0) {
-            throw new Exception("Invalid penalty for " + questionText);
+            throw new Exception("Invalid penalty for " + qText);
         }
-        return new Question(questionText, choices, correctAnswer,
+        return new Question(qText, choices, correctAnswer,
                             maxMarks, penalty);
     }
 }
